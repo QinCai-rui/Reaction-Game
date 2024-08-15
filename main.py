546c1ebb350b63f05059ae37aa5ad827e43454a3
QinCai-rui <140027854+QinCai-rui@users.noreply.github.com>
2024-08-15 17:09:13 +1200

    Update main.py

--- main.py
+++ main.py
@@ -30,13 +30,15 @@ import time
 # Initialise the LED
 LED = Pin(26, Pin.OUT)
 
+
 # Initialise the buttons
-BTN_RED = Pin(19, Pin.IN, Pin.PULL_DOWN)
-BTN_GREEN = Pin(18, Pin.IN, Pin.PULL_DOWN)
+BTN_L = Pin(19, Pin.IN, Pin.PULL_DOWN)
+BTN_R = Pin(18, Pin.IN, Pin.PULL_DOWN)
 
 # Initialise I2C for the SSD1306 OLED display
 i2c = I2C(0, scl=Pin(17), sda=Pin(16))
 oled = ssd1306.SSD1306_I2C(128, 64, i2c)
+
 
 def test():
     LED.value(1)
@@ -48,21 +50,45 @@ def test():
     oled.text("TEST", 0, 0)
     oled.show()
   
-    while 1: 
-    if BTN_RED.value() == 1: 
-        LED.value(1)
-        time.sleep(1)
-    LED.value(0)
+    while 1:
+        if BTN_L.value() == 1:
+            LED.value(1)
+            time.sleep(1)
+        LED.value(0)
+        time.sleep(0.125)
 
 def select_player_mode():
     oled.fill(0)
-    oled.text("Select Player Mode", 0, 0)
+    oled.text("Select Mode", 0, 0)
     oled.show()
-    time.sleep(0.75)
-    oled.text("Press the left button for 1 player", 0, 0)
+    time.sleep(1.5)
+    oled.text("L btn = 1 person", 0, 20)
+    oled.show()
+    time.sleep(1.25)
+    oled.text("R btn = 2 people", 0, 30)
     oled.show()
 
-LED.value(1)
+    while 1:
+        #global singleMode
+        if BTN_L.value() == 1:
+            singleMode = True
+            break
+        if BTN_R.value() == 1:
+            singleMode = False
+            break
+        time.sleep(0.125)
 
-test()
+    if singleMode: 
+        mode = '1 player mode'
+    else: 
+        mode = '2 players mode'
+
+    oled.fill(0)
+    oled.text("The mode you chose", 0, 0)
+    oled.text(f"is {mode}") # to-do
+    oled.show()
+
+
+
+#test()
 select_player_mode()
